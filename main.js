1129870c2327bf9431831624d4e3501ac3ce8579
Claudio Churque <89611166+Churque@users.noreply.github.com>
2023-04-28 17:32:06 -0400

    Add files via upload
    combate

--- main.js
+++ main.js
@@ -32,6 +32,10 @@ function generarVelocidad(){
     return Math.floor(Math.random() * 10) + 1;
 }
 
+function comprobarNombre(){
+
+}
+
 
 function generarPersonajeAleatorio(){
     const nombre = 'Personaje_'+getNumeroRandom(100);
@@ -48,12 +52,77 @@ function generarPersonajeAleatorio(){
     primerAtaque,
     segundoAtaque,
     vida,
-    velocidad
+    velocidad,
+    ataquesFallados : 0
   };
+}
 
+function elegirAtaque(personaje){
+    return Math.random() < 0.5 ? personaje.primerAtaque : personaje.segundoAtaque;
+}
+
+
+function ataque (atacante, defensa, numTurno) {
+    let terminarCombate = false;
+    const ataque = elegirAtaque(atacante);
+    const presicion = Math.floor(Math.random() * (100 - 1 + 1)) + 1;;
+    console.log(atacante)
+    if (ataque.accuracy >= presicion) {
+        defensa.vida -= ataque.damage;
+        if(defensa.vida <= 0){
+            console.log("termina combate")
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+      atacante.ataquesFallados++;
+      return false;
+    }
+
+    return terminarCombate;
+}
+
+function prioridadTurnoCombate (personaje1, personaje2){
+    let atacante, defensa,numTurno = 0,terminarCombate = false;
+  if (personaje1.velocidad > personaje2.velocidad) {
+    atacante = personaje1;
+    defensa = personaje2;
+  } else if (personaje2.velocidad > personaje1.velocidad) {
+    atacante = personaje2;
+    defensa = personaje1;
+  } else {
+    if (Math.random() < 0.5) {
+        atacante = personaje1;
+        defensa = personaje2;
+    } else {
+        atacante = personaje2;
+        defensa = personaje1;
+    }
+  }
+    
+  
+  do{
+    numTurno++
+    terminarCombate = ataque(atacante,defensa,numTurno);
+    if(defensa.vida <= 0){
+        break;
+    }
+    
+    terminarCombate = ataque(defensa,atacante,numTurno);
+    if(atacante.vida <=0){
+        break;
+    }
+  } while(true);
 
 }
 
+function comenzarCombate(personaje1,personaje2){
+    prioridadTurnoCombate(personaje1,personaje2);
+}
+
+
+
 let personaje1 = generarPersonajeAleatorio();
-console.log(personaje1);
-//console.log(types[getNumeroRandom(types.length)]);
+let personaje2 = generarPersonajeAleatorio();
+comenzarCombate(personaje1,personaje2);
